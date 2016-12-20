d7f6c14897a3d9fd8e2534a8e7dfef2d706db1de
SarahJaine <sarahjaine.sz@gmail.com>
2016-12-19 19:52:32 -0500

    added templates, urls

--- sayit/urls.py
+++ sayit/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from sayit.views import HomePageView
+from sayit.views import HomeView, HowView, MusiciansView, OrderView
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', HomePageView.as_view(), name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    
-
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^$', AboutView.as_view(), name='about'),
+    url(r'^$', MusiciansView.as_view(), name='musicians'),
+    url(r'^$', OrderView.as_view(), name='order'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- sayit/views.py
+++ sayit/views.py
@@ -3,5 +3,17 @@ from __future__ import unicode_literals
 from django.views.generic import TemplateView
 
 
-class HomePageView(TemplateView):
+class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class MusiciansView(TemplateView):
+    template_name = 'musicians.html'
+
+
+class OrderView(TemplateView):
+    template_name = 'order.html'
