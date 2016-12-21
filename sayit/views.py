1d5d87285454dd4cb5559fb5ee4f4c8cc48345d3
SarahJaine <sarahjaine.sz@gmail.com>
2016-12-20 19:43:53 -0500

    added main margins, added example model and copy to homepage

--- sayit/views.py
+++ sayit/views.py
@@ -1,10 +1,13 @@
 from __future__ import unicode_literals
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+from sayit.models import Example
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'home.html'
+    model = Example
+    context_object_name = 'examples'
 
 
 class HowView(TemplateView):
