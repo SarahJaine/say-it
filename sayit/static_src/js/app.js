dd69ebfd29aa37f7e8cf1adc87088bffba95abb3
SarahJaine <sarahjaine.sz@gmail.com>
2017-01-08 21:00:05 -0500

    First real pass at overall design

--- sayit/static_src/js/app.js
+++ sayit/static_src/js/app.js
@@ -5,7 +5,3 @@ import 'foundation-responsivetoggle'
 
 // initialize foundation
 $(document).foundation()
-// example
-const dateDisplayEl = document.createElement('div')
-dateDisplayEl.innerHTML = new Date()
-document.body.appendChild(dateDisplayEl)
