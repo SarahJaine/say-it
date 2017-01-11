c81ac8208997944c52357744ba7a7cf397dd5c19
SarahJaine <sarahjaine.sz@gmail.com>
2017-01-11 10:40:37 -0500

    re-required fields, pep8

--- sayit/forms.py
+++ sayit/forms.py
@@ -22,41 +22,40 @@ class OrderForm(UnstyledForm):
 		('B', 'Slow ballad'),
 		('O', 'Other')
 	]
-	name = forms.CharField(max_length=50,
-		required=False,
+	name = forms.CharField(
+		max_length=50,
 		widget=forms.TextInput(
 			attrs={'placeholder': 'Jane Doe'}))
 	email = forms.EmailField(
-		required=False,
 		widget=forms.TextInput(
 			attrs={'placeholder': 'jdoe@example.com'}))
-	phone = forms.CharField(max_length=14,
+	phone = forms.CharField(
+		max_length=14,
 		widget=forms.TextInput())
-	phone_hidden = forms.CharField(max_length=18,
-		required=False, label="",
+	phone_hidden = forms.CharField(
+		label="", max_length=18, required=False,
 		widget=forms.HiddenInput())
 	product = forms.ChoiceField(choices=PRODUCT_CHOICES)
 	language = forms.ChoiceField(choices=LANG_CHOICES)
-	occasion = forms.CharField(max_length=50,
-		required=False,
+	occasion = forms.CharField(
+		max_length=50,
 		widget=forms.TextInput(
 			attrs={'placeholder': 'Birthday'}))
-	date = forms.DateField(label='Date of Occasion',
-		required=False,
+	date = forms.DateField(
+		label='Date of Occasion',
 		widget=forms.DateInput(
 			attrs={'placeholder': '10/29/2017'}))
-	recipient = forms.CharField(label="Recipient's Name", max_length=50,
-		required=False,
+	recipient = forms.CharField(
+		label="Song Recipient's Name", max_length=50,
 		widget=forms.TextInput(
 			attrs={'placeholder': 'Jamal Doe'}))
-	relationship = forms.CharField(label="Relationship to Recipient",
-		required=False,
-		max_length=50,
+	relationship = forms.CharField(
+		label="Your Relationship to Recipient", max_length=50,
 		widget=forms.TextInput(
 			attrs={'placeholder': 'Best Friend'}))
 	style = forms.ChoiceField(choices=STYLE_CHOICES)
-	message = forms.CharField(label='Any Additional Info', max_length=5000,
-		required=False,
+	message = forms.CharField(
+		label='Any Additional Info', max_length=5000, required=False,
 		widget=forms.Textarea(
-			attrs={'placeholder':
-				'I would love for us to be able to dance to this song!'}))
+			attrs={
+				'placeholder': 'I would love for us to be able to dance to this song!'}))
