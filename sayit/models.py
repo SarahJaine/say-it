b717deaa45b480f1e9ae075cfff4ee4e09977d64
SarahJaine <sarahjaine.sz@gmail.com>
2017-10-01 00:18:03 -0500

    add songs option to musician profile

--- sayit/models.py
+++ sayit/models.py
@@ -2,44 +2,48 @@ from django.db import models
 
 
 class Example(models.Model):
-	title = models.CharField(max_length=80)
-	product_type = models.CharField(max_length=100)
-	review = models.TextField(max_length=500, unique=True)
-	media_link = models.URLField(blank=True, null=True, max_length=100)
-	audio_image = models.ImageField(blank=True, null=True, upload_to='examples/')
-	media = models.FileField(blank=True, null=True, upload_to='examples/')
-	order = models.IntegerField(default=0)
-	STATUS_CHOICES = (
-		('d', 'Draft'),
-		('p', 'Published'),
-	)
-	status = models.CharField(max_length=1, choices=STATUS_CHOICES,
-							  default='d')
+    title = models.CharField(max_length=80)
+    product_type = models.CharField(max_length=100)
+    review = models.TextField(max_length=500, unique=True)
+    media_link = models.URLField(blank=True, null=True, max_length=100)
+    audio_image = models.ImageField(blank=True, null=True, upload_to='examples/')
+    media = models.FileField(blank=True, null=True, upload_to='examples/')
+    order = models.IntegerField(default=0)
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,
+                              default='d')
 
-	class Meta:
-		ordering = ["order"]
+    class Meta:
+        ordering = ["order"]
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Musician(models.Model):
-	name = models.CharField(max_length=80, unique=True)
-	position = models.CharField(max_length=100)
-	musical_styles = models.CharField(max_length=100)
-	image = models.ImageField(blank=True, null=True, upload_to='musicians/')
-	bio = models.TextField(max_length=500, unique=True)
-	profile_url = models.URLField(max_length=100)
-	order = models.IntegerField(default=0)
-	STATUS_CHOICES = (
-		('d', 'Draft'),
-		('p', 'Published'),
-	)
-	status = models.CharField(max_length=1, choices=STATUS_CHOICES,
-							  default='d')
+    name = models.CharField(max_length=80, unique=True)
+    position = models.CharField(max_length=100)
+    musical_styles = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True, upload_to='musicians/')
+    bio = models.TextField(max_length=500, unique=True)
+    profile_url = models.URLField(blank=True, null=True, max_length=100)
+    song_url_1 = models.URLField(blank=True, null=True, max_length=100)
+    song_name_1 = models.CharField(blank=True, null=True, max_length=80)
+    song_url_2 = models.URLField(blank=True, null=True, max_length=100)
+    song_name_2 = models.CharField(blank=True, null=True, max_length=80)
+    order = models.IntegerField(default=0)
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,
+                              default='d')
 
-	class Meta:
-		ordering = ["order"]
+    class Meta:
+        ordering = ["order"]
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
